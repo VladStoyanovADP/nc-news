4b99ce8310f13102c183843367b5ebfa99b61828
Vlad <vladstoyanoff@gmail.com>
2023-01-16 16:22:23 +0000

    tests

--- db/app/app.test.js
+++ db/app/app.test.js
@@ -19,32 +19,86 @@ describe("get /api/topics", () => {
   test("returns correct body", () => {
     return request(app)
       .get("/api/topics")
-      .then((res) => {
-        const topic = res.body.topics[0];
-        expect(topic).toHaveProperty("description");
-        expect(topic).toHaveProperty("slug");
+      .then((res) =>
+      {
+        res.body.topics.forEach(topic =>
+        {
+          expect(topic).toHaveProperty("description");
+          expect(topic).toHaveProperty("slug");
+        })
       });
   });
+
+    test("response is not empty", () => {
+      return request(app)
+        .get("/api/topics")
+        .then((res) => {
+          expect(res.body.topics.length).toBeGreaterThan(0);
+        });
+    });
 });
 
 describe("get /api/articles", () => {
+
   test("responds with status 200", () => {
     return request(app).get("/api/articles").expect(200);
   });
+
   test("returns correct body", () => {
     return request(app)
       .get("/api/articles")
-      .then((res) => {
-        const article = res.body.articles[0];
-        expect(article).toHaveProperty("title");
-        expect(article).toHaveProperty("topic");
-        expect(article).toHaveProperty("author");
-        expect(article).toHaveProperty("article_id");
-        expect(article).toHaveProperty("body");
-        expect(article).toHaveProperty("created_at");
-        expect(article).toHaveProperty("votes");
-        expect(article).toHaveProperty("article_img_url");
-        expect(article).toHaveProperty("comment_count")
+      .then((res) =>
+      {
+        res.body.articles.forEach(article =>
+        {
+          expect(article).toHaveProperty("title");
+          expect(article).toHaveProperty("topic");
+          expect(article).toHaveProperty("author");
+          expect(article).toHaveProperty("article_id");
+          expect(article).toHaveProperty("body");
+          expect(article).toHaveProperty("created_at");
+          expect(article).toHaveProperty("votes");
+          expect(article).toHaveProperty("article_img_url");
+          expect(article).toHaveProperty("comment_count")
+        })
       });
   });
+
+  test("response is not empty", () => {
+    return request(app)
+      .get("/api/articles")
+      .then((res) => {
+        expect(res.body.articles.length).toBeGreaterThan(0);
+      });
+  });
+
+  test("response has correct types", () => {
+    return request(app)
+      .get("/api/articles")
+      .then((res) =>
+      {
+        res.body.articles.forEach(article =>
+        {
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.body).toBe("string");
+          expect(typeof article.created_at).toBe('string');
+          expect(typeof article.votes).toBe("number");
+          expect(typeof article.article_img_url).toBe("string");
+          expect(typeof article.comment_count).toBe("string");
+        })
+      });
+  });
+
+   test("response has correct types", () => {
+     return request(app)
+       .get("/api/articles")
+       .then((res) => {
+          expect(res.body.articles).toBeSortedBy("created_at", {
+            descending: true
+          });
+       });
+   });
 });
