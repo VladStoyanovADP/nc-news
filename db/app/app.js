082191e21d59dd6219c20d0dc3e1ef83b4ad7440
Vlad <vladstoyanoff@gmail.com>
2023-01-17 14:38:03 +0000

    errorHandler file

--- db/app/app.js
+++ db/app/app.js
@@ -1,5 +1,6 @@
 const { getTopics, getArticles } = require("./controllers.js");
 const express = require("express");
+const { postgresErr, customErr } = require("./errorHandlers");
 const app = express();
 
 app.use(express.json());
@@ -7,5 +8,8 @@ app.use(express.json());
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
+
+app.use(postgresErr);
+app.use(customErr);
 
 module.exports = app;
---
+++ db/app/errorHandlers.js
@@ -0,0 +1,20 @@
+const postgresErr = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
+  }
+};
+
+const customErr = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+
+module.exports = {
+  postgresErr,
+  customErr,
+};
