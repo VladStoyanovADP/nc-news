ea14b329cdf879d0e2f9a61709fbbf3bf5827915
Vlad <vladstoyanoff@gmail.com>
2023-01-17 12:24:53 +0000

    ncnews-6 and error handling for 5 and 6

--- db/app/app.js
+++ db/app/app.js
@@ -1,4 +1,10 @@
-const { getTopics, getArticles } = require("./controllers.js");
+const {
+  getTopics,
+  getArticles,
+  getArticleByID,
+  getCommentsOfArticle
+} = require("./controllers.js");
+const { postgresErr, customErr } = require("./errorHandlers");
 const express = require("express");
 const app = express();
 
@@ -7,5 +13,16 @@ app.use(express.json());
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
+
+app.get("/api/articles/:id", getArticleByID);
+
+app.get("/api/articles/:id/comments", getCommentsOfArticle);
+
+app.use(postgresErr);
+app.use(customErr);
+
+app.all("*", (req, res) => {
+  app.status(404).send({ msg: "Bad Request" });
+});
 
 module.exports = app;
--- db/app/app.test.js
+++ db/app/app.test.js
@@ -32,7 +32,6 @@ describe("get /api/topics", () => {
         .get("/api/topics")
         .then((res) =>
         {
-          console.log(index)
           expect(res.body.topics.length).toBe(index.topicData.length);
         });
     });
@@ -91,14 +90,138 @@ describe("get /api/articles", () => {
         })
       });
   });
+});
 
-   test("checks whether the articles are ordered by date created", () => {
-     return request(app)
-       .get("/api/articles")
-       .then((res) => {
-          expect(res.body.articles).toBeSortedBy("created_at", {
-            descending: true
-          });
-       });
-   });
+describe("get /api/articles/:id", () => {
+  test("responds with status 200", () => {
+    return request(app).get("/api/articles/3").expect(200);
+  });
+
+  test("checks whether each article has the correct keys", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .then((res) => {
+        let article = res.body.article;
+        expect(article).toHaveProperty("title");
+        expect(article).toHaveProperty("topic");
+        expect(article).toHaveProperty("author");
+        expect(article).toHaveProperty("article_id");
+        expect(article).toHaveProperty("body");
+        expect(article).toHaveProperty("created_at");
+        expect(article).toHaveProperty("votes");
+        expect(article).toHaveProperty("article_img_url");
+      });
+  });
+
+  test("checks the type of the values", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .then((res) => {
+        let article = res.body.article;
+        expect(typeof article.title).toBe("string");
+        expect(typeof article.topic).toBe("string");
+        expect(typeof article.author).toBe("string");
+        expect(typeof article.article_id).toBe("number");
+        expect(typeof article.body).toBe("string");
+        expect(typeof article.created_at).toBe("string");
+        expect(typeof article.votes).toBe("number");
+        expect(typeof article.article_img_url).toBe("string");
+      });
+  });
+  test("checks whether the articles are ordered by date created", () => {
+    return request(app)
+      .get("/api/articles")
+      .then((res) => {
+        expect(res.body.articles).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
+
+  test("400: Should respond with Bad Request for invalid id", () => {
+    const id = "dog";
+
+    return request(app)
+      .get(`/api/articles/${id}`)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+      });
+  });
+
+  test("404: Should respond with Not Found for id not in database ", () => {
+    return request(app)
+      .get(`/api/articles/999`)
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Not Found");
+      });
+  });
+});
+
+
+describe("get /api/articles/:id/comments", () => {
+  test("responds with status 200", () => {
+    return request(app).get("/api/articles/3/comments").expect(200);
+  });
+
+  test("checks whether each article has the correct keys", () => {
+    return request(app)
+      .get("/api/articles/3/comments")
+      .then((res) => {
+        res.body.comments.forEach((comment) => {
+          expect(comment).toHaveProperty("comment_id");
+          expect(comment).toHaveProperty("votes");
+          expect(comment).toHaveProperty("created_at");
+          expect(comment).toHaveProperty("author");
+          expect(comment).toHaveProperty("body");
+          expect(comment).toHaveProperty("article_id");
+        });
+      });
+  });
+
+  test("checks the type of the values", () => {
+    return request(app)
+      .get("/api/articles/3/comments")
+      .then((res) => {
+        res.body.comments.forEach((comment) => {
+          expect(typeof comment.comment_id).toBe("number");
+          expect(typeof comment.votes).toBe("number");
+          expect(typeof comment.created_at).toBe("string");
+          expect(typeof comment.author).toBe("string");
+          expect(typeof comment.body).toBe("string");
+          expect(typeof comment.article_id).toBe("number");
+        });
+      });
+  });
+
+  test("checks whether the articles are ordered by date created", () => {
+    return request(app)
+      .get("/api/articles/3/comments")
+      .then((res) => {
+        expect(res.body.comments).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
+
+  test("400: Should respond with Bad Request for invalid id", () => {
+    const id = "dog";
+
+    return request(app)
+      .get(`/api/articles/${id}/comments`)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Bad Request");
+      });
+  });
+
+  test("404: Should respond with Not Found for id not in database ", () => {
+    return request(app)
+      .get(`/api/articles/999/comments`)
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Not Found");
+      });
+  });
 });
